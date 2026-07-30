11c265c1b55e32bbd8ac159736e527069945b0df
Lrht-llw <2179725762@qq.com>
2026-07-30 09:41:33 +0800

    feat：多开检测

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Configuration;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +9,25 @@ namespace KfuPet_Tool
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex? _mutex;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            const string mutexName = "KfuPet-Tool_SingleInstance";
+
+            _mutex = new Mutex(true, mutexName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("KfuPet-Tool 已经在运行中，不能同时打开多个工具。", "提示",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                Shutdown();
+                return;
+            }
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 
 }
